11a14e4b5bd4a9be7cad9abb3b6ffd511775bb83
nemesek <dnemesek@gmail.com>
2021-12-01 17:15:21 -0600

    Delete config.py

--- RedditSentiment/config.py
+++
@@ -1,4 +0,0 @@
-messari_api_key = '7b55df85-5ad7-40b7-9db6-e87fe54fda99'
-glassnode_api_key = '21eIwRsMUN3gcCLaxxxwzvPR4sQ'
-reddit_client_id = 'snvxbdQiMNm03isjrqhkyw'
-reddit_client_secret = '9SLK8uG7DQ7B5ZtisuPTk3WtL8jGEw'
