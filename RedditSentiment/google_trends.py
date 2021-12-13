19c7820b6a7bac30caf0bb09c32e026ac7602d4a
nemesek <dnemesek@gmail.com>
2021-12-13 12:31:28 -0600

    more google trends

--- RedditSentiment/google_trends.py
+++ RedditSentiment/google_trends.py
@@ -4,15 +4,20 @@ from pytrends.request import TrendReq
 def get_trends():
     print('getting trends')
     pytrend = TrendReq()
-    kw_list = ["Blockchain"]
-    #pytrend.build_payload(kw_list=[‘Taylor Swift’])
+    kw_list = ["Blockchain", "Bitcoin", "Ethereum"]
     pytrend.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
-
-    #pytrend.build_payload()
+    related_queries = pytrend.related_queries()
+    print(related_queries.values())
     # Interest by Region
-    df = pytrend.interest_by_region()
-    print(df.head(10))
+    df_region = pytrend.interest_by_region()
+    print(df_region.head(10))
     # Get Google Hot Trends data
-    #df2 = pytrend.trending_searches(pn=’united_states’)
-    df2 = pytrend.trending_searches(pn='united_states')
-    print(df2.head())
+    # df_trending = pytrend.trending_searches(pn='united_states')
+    df_trending = pytrend.trending_searches()
+    print(df_trending.head(100))
+    print('****************interest over time****************')
+    df_interest_time =pytrend.interest_over_time()
+    print(df_interest_time.tail(5))
+    print('****************top charts****************')
+    df_top_charts = pytrend.top_charts(2021, hl='en-US', tz=300, geo='GLOBAL')
+    print(df_top_charts.head(100))
